428e2ca950caa53560a237e4bf9519a229e5619b
rockderick <rockderick@gmail.com>
2017-06-01 12:54:03 -0500

    First commit

---
+++ app/src/main/java/com/rockderick/guitarshopcard/GuitarShopActivity.java
@@ -0,0 +1,13 @@
+package com.rockderick.guitarshopcard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuitarShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guitar_shop);
+    }
+}
